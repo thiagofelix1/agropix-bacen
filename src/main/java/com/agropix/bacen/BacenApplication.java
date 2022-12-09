7a6feec38eb07738d70a8c994e1ba008fa4778c3
thiagofelix1 <thiagofelixdasilva099@gmail.com>
2022-12-09 15:57:14 -0400

    first commit

---
+++ src/main/java/com/agropix/bacen/BacenApplication.java
@@ -0,0 +1,13 @@
+package com.agropix.bacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agropix/bacen/BacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agropix.bacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
