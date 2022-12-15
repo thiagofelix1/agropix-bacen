b8ad97bc0f8c009e4d328261a235c34fce6c819e
thiagofelix1 <thiagofelixdasilva099@gmail.com>
2022-12-15 10:56:29 -0400

    refactor: alterando deploy

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tipo_chave (id, tipo) VALUES ('74cd90d5-11c5-483f-a033-4062e2c4e683', 'ALEATORIA');
+INSERT INTO tipo_chave (id, tipo) VALUES ('cf58ff8e-66ae-4541-9f5c-cc5613264a07', 'EMAIL');
+INSERT INTO tipo_chave (id, tipo) VALUES ('1626ad1a-5977-4516-bb04-9fbccf07f1cb', 'CELULAR');
+INSERT INTO tipo_chave (id, tipo) VALUES ('56f4a424-3c5a-4d10-bbfb-9a5362639792', 'CPF');
