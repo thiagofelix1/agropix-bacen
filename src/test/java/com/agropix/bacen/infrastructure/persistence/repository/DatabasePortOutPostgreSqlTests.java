a23edc008dafef49d9285e2a41d32e6cdd64358d
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-12-16 19:48:37 -0300

    finalizando testes do metodo save

--- src/test/java/com/agropix/bacen/infrastructure/persistence/repository/DatabasePortOutPostgreSqlTests.java
+++ src/test/java/com/agropix/bacen/infrastructure/persistence/repository/DatabasePortOutPostgreSqlTests.java
@@ -5,7 +5,9 @@ import com.agropix.bacen.domain.entities.ChavePix;
 import com.agropix.bacen.domain.entities.Conta;
 import com.agropix.bacen.domain.entities.PessoaFisica;
 import com.agropix.bacen.domain.enums.TipoChavePix;
+import com.agropix.bacen.infrastructure.persistence.model.PessoaFisicaPersistenceModel;
 import com.agropix.bacen.infrastructure.persistence.model.TipoChavePixPersistenceModel;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class DatabasePortOutPostgreSqlTests {
+class DatabasePortOutPostgreSqlTests {
 
     @InjectMocks
     private DatabasePortOutPostgreSql databasePortOutPostgreSql;
@@ -39,38 +41,87 @@ public class DatabasePortOutPostgreSqlTests {
     @Mock
     private BancoRepository bancoRepository;
 
-    @Test
-    void deveLancarExcecaoAoNaoEncontrarTipoChave() {
-        when(tipoChavePixRepository.findByTipo(anyString())).thenReturn(Optional.empty());
+    @Nested
+    class SaveTests {
+        @Test
+        void deveLancarExcecaoAoNaoEncontrarTipoChave() {
+            when(tipoChavePixRepository.findByTipo(anyString())).thenReturn(Optional.empty());
 
-        ChavePix chavePix = mockChavePix();
+            ChavePix chavePix = mockChavePix();
 
-        assertThrows(RuntimeException.class, () -> databasePortOutPostgreSql.save(chavePix));
-    }
+            assertThrows(RuntimeException.class, () -> databasePortOutPostgreSql.save(chavePix));
+        }
 
-    @Test
-    void deveBuscarPessoaFisicaSeExistirPorCpf() {
-        when(tipoChavePixRepository.findByTipo(anyString())).thenReturn(Optional.of(new TipoChavePixPersistenceModel()));
-        when(pessoaFisicaRepository.existsByCpf(any())).thenReturn(true);
+        @Test
+        void deveBuscarPessoaFisicaSeExistirPorCpf() {
+            when(tipoChavePixRepository.findByTipo(anyString())).thenReturn(Optional.of(new TipoChavePixPersistenceModel()));
+            when(pessoaFisicaRepository.existsByCpf(any())).thenReturn(true);
 
-        assertDoesNotThrow(() -> databasePortOutPostgreSql.save(mockChavePix()));
+            assertDoesNotThrow(() -> databasePortOutPostgreSql.save(mockChavePix()));
 
-        verify(pessoaFisicaRepository, times(1)).findByCpf(any());
-    }
+            verify(pessoaFisicaRepository, times(1)).findByCpf(any());
+        }
 
-    @Test
-    void deveSalvarPessoaFisicaSeNaoExistirPorCpf() {
-        when(tipoChavePixRepository.findByTipo(anyString())).thenReturn(Optional.of(new TipoChavePixPersistenceModel()));
-        when(pessoaFisicaRepository.existsByCpf(any())).thenReturn(false);
+        @Test
+        void deveSalvarPessoaFisicaSeNaoExistirPorCpf() {
+            when(tipoChavePixRepository.findByTipo(anyString())).thenReturn(Optional.of(new TipoChavePixPersistenceModel()));
+            when(pessoaFisicaRepository.existsByCpf(any())).thenReturn(false);
 
-        assertDoesNotThrow(() -> databasePortOutPostgreSql.save(mockChavePix()));
+            assertDoesNotThrow(() -> databasePortOutPostgreSql.save(mockChavePix()));
 
-        verify(pessoaFisicaRepository, times(1)).saveAndFlush(any());
-    }
+            verify(pessoaFisicaRepository, times(1)).saveAndFlush(any());
+        }
 
-    @Test
-    void deveBuscarContaPorNumeroSeExistir() {
+        @Test
+        void deveBuscarContaSeExistirPorNumeroConta() {
+            when(tipoChavePixRepository.findByTipo(anyString())).thenReturn(Optional.of(new TipoChavePixPersistenceModel()));
+            when(contaRepository.existsByNumeroConta(any())).thenReturn(true);
 
+            assertDoesNotThrow(() -> databasePortOutPostgreSql.save(mockChavePix()));
+
+            verify(contaRepository, times(1)).findByNumeroConta(any());
+        }
+
+        @Test
+        void deveSalvarContaSeNaoExistirPorNumeroConta() {
+            PessoaFisica pessoaFisicaEsperada = mockPessoaFisica();
+            PessoaFisicaPersistenceModel pessoaFisicaPersistenceModel = PessoaFisicaPersistenceModel.fromEntity(pessoaFisicaEsperada);
+
+            when(tipoChavePixRepository.findByTipo(anyString())).thenReturn(Optional.of(new TipoChavePixPersistenceModel()));
+            when(pessoaFisicaRepository.existsByCpf(any())).thenReturn(true);
+            when(pessoaFisicaRepository.findByCpf(any())).thenReturn(pessoaFisicaPersistenceModel);
+            when(contaRepository.existsByNumeroConta(any())).thenReturn(false);
+
+            ChavePix chavePix = assertDoesNotThrow(() -> databasePortOutPostgreSql.save(mockChavePix()));
+
+            PessoaFisica pessoaFisicaRetornada = chavePix.getConta().getTitular();
+
+            assertEquals(pessoaFisicaEsperada.getNome(), pessoaFisicaRetornada.getNome());
+            assertEquals(pessoaFisicaEsperada.getCpf(), pessoaFisicaRetornada.getCpf());
+            assertEquals(pessoaFisicaEsperada.getTelefone(), pessoaFisicaRetornada.getTelefone());
+            assertEquals(pessoaFisicaEsperada.getEmail(), pessoaFisicaRetornada.getEmail());
+            verify(contaRepository, times(1)).saveAndFlush(any());
+        }
+
+        @Test
+        void deveBuscarBancoSeExistirPorCodigo() {
+            when(tipoChavePixRepository.findByTipo(anyString())).thenReturn(Optional.of(new TipoChavePixPersistenceModel()));
+            when(bancoRepository.existsByCodigo(any())).thenReturn(true);
+
+            assertDoesNotThrow(() -> databasePortOutPostgreSql.save(mockChavePix()));
+
+            verify(bancoRepository, times(1)).getByCodigo(any());
+        }
+
+        @Test
+        void deveSalvarBancoSeNaoExistirPorCodigo() {
+            when(tipoChavePixRepository.findByTipo(anyString())).thenReturn(Optional.of(new TipoChavePixPersistenceModel()));
+            when(bancoRepository.existsByCodigo(any())).thenReturn(false);
+
+            assertDoesNotThrow(() -> databasePortOutPostgreSql.save(mockChavePix()));
+
+            verify(bancoRepository, times(1)).saveAndFlush(any());
+        }
     }
 
     private Banco mockBanco() {
